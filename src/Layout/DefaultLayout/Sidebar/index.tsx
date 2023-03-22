c0e61f566d3a14d22aeb57c3aa3daa2520b5f758
s1mple0o <nam0917900351a@gmail.com>
2023-03-22 17:34:39 +0700

    setup_default_layout_and_config_var_css

--- src/Layout/DefaultLayout/Sidebar/index.tsx
+++ src/Layout/DefaultLayout/Sidebar/index.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
 
+
+const cx = classNames.bind(styles);
 
 const Sidebar = () => {
-    return (<div>Sidebar</div>);
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side Bar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
--- src/Layout/DefaultLayout/index.tsx
+++ src/Layout/DefaultLayout/index.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Outlet } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+
 
 import Header from "../components/Header";
 import Sidebar from "./Sidebar";
 
 
+const cx = classNames.bind(styles);
+
 const DefaultLayout = () => {
-    return (<div>
-        <Header />
-        <div className="container">
-            <Sidebar />
-            <div className="content">
-                <Outlet />
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>
+                    <Outlet />
+                </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 export default DefaultLayout;
